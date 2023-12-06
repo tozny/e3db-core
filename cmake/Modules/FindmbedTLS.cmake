b8605d2e0e5e661b2ea960dc58204e3a3d55d8f9
Katie Rosas <katie@tozny.com>
2023-12-06 14:28:24 -0800

    Add all paths

--- cmake/Modules/FindmbedTLS.cmake
+++ cmake/Modules/FindmbedTLS.cmake
@@ -2,12 +2,12 @@
 # This module looks for the mbedTLS library.
 find_path(MBEDTLS_INCLUDE_DIR mbedtls/base64.h
   PATH_SUFFIXES include mbedtls
-  PATHS /usr/local/opt/mbedtls/include /usr/local/Cellar/mbedtls/3.5.1/include
+  PATHS /usr/local/opt/mbedtls/include /usr/local/Cellar/mbedtls/3.5.1/include /opt/homebrew/Cellar/mbedtls/3.5.1/include
 )
 find_library(MBEDTLS_LIBRARY
   NAMES mbedtls mbedcrypto
   PATH_SUFFIXES lib
-  PATHS /usr/local/opt/mbedtls/lib  /usr/local/Cellar/mbedtls/3.5.1/lib
+  PATHS /usr/local/opt/mbedtls/lib  /usr/local/Cellar/mbedtls/3.5.1/lib /opt/homebrew/Cellar/mbedtls/3.5.1/lib
 )
 if (MBEDTLS_INCLUDE_DIR AND MBEDTLS_LIBRARY)
   set(MBEDTLS_FOUND TRUE)
