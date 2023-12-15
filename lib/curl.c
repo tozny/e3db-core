af1ab881dcd5bcff47122cd1f1acd88900e907a5
Katie Rosas <katie@tozny.com>
2023-12-15 15:48:23 -0800

    remove extra line

--- lib/curl.c
+++ lib/curl.c
@@ -24,7 +24,7 @@
 #include "mbedtls/ctr_drbg.h"
 
 int MAX_BUFFER_SIZE = 8192;
-int MAX_REQUEST_SIZE = 8192;
+int MAX_REQUEST_SIZE = 10000;
 int MAX_HEADER_SIZE = 8192;
 // Define a structure to hold the response data
 struct ResponseData
@@ -129,7 +129,6 @@ int curl_run_op(E3DB_Op *op)
 
 			const char *method = E3DB_Op_GetHttpMethod(op);
 			E3DB_HttpHeaderList *headers = E3DB_Op_GetHttpHeaders(op);
-
 			struct curl_slist *chunk = NULL;
 			E3DB_HttpHeader *header = E3DB_HttpHeaderList_GetFirst(headers);
 
@@ -138,11 +137,11 @@ int curl_run_op(E3DB_Op *op)
 				char *header_text = malloc(strlen(E3DB_HttpHeader_GetName(header)) + strlen(E3DB_HttpHeader_GetValue(header)) + 3);
 				sprintf(header_text, "%s: %s", E3DB_HttpHeader_GetName(header), E3DB_HttpHeader_GetValue(header));
 				chunk = curl_slist_append(chunk, header_text);
-				free(header_text);
+				// free(header_text);
 
 				header = E3DB_HttpHeader_GetNext(header);
 			}
-
+			printf("Header in curl  %s \n", chunk->data);
 			if (!strcmp(method, "POST"))
 			{
 				const char *post_body = E3DB_Op_GetHttpBody(op);
@@ -363,10 +362,12 @@ int mbedtls_run_op(E3DB_Op *op)
 		if (E3DB_Op_IsHttpState(op))
 		{
 			const char *method = E3DB_Op_GetHttpMethod(op);
+
 			printf("METHOD being processed %s\n", method);
 
 			E3DB_HttpHeaderList *headers = E3DB_Op_GetHttpHeaders(op);
-
+			size_t lengthOfhEders = E3DB_HttpHeaderList_GetLength(headers);
+			printf("HEADERS LENGTH %zu", lengthOfhEders);
 			E3DB_HttpHeader *header = E3DB_HttpHeaderList_GetFirst(headers);
 
 			// Allocate memory for headers_string and request
@@ -390,11 +391,9 @@ int mbedtls_run_op(E3DB_Op *op)
 				}
 				sprintf(header_text, "%s: %s\r\n", E3DB_HttpHeader_GetName(header), E3DB_HttpHeader_GetValue(header));
 				strcat(headers_string, header_text);
-				free(header_text); // Free the allocated memory
+				// free(header_text); // Free the allocated memory
 				header = E3DB_HttpHeader_GetNext(header);
 			}
-
-			printf("All headers as a string: %s\n", headers_string);
 
 			if (!strcmp(method, "POST"))
 			{
@@ -406,8 +405,8 @@ int mbedtls_run_op(E3DB_Op *op)
 						   "POST %s HTTP/1.1\r\n"
 						   "Host: api.e3db.com\r\n" // Use only the hostname
 						   "Content-Length: %zu\r\n"
-						   "%s\r\n"  // Authorization header
-						   "\r\n%s", // Request body
+						   "%s\r\n" // Authorization header
+						   "%s",    // Request body
 						   E3DB_Op_GetHttpUrl(op), strlen(post_body), headers_string, post_body);
 
 				printf("Ret size %d", ret);
@@ -425,6 +424,7 @@ int mbedtls_run_op(E3DB_Op *op)
 			else if (!strcmp(method, "GET"))
 			{
 				printf("URL length: %zu\n", strlen(E3DB_Op_GetHttpUrl(op)));
+				printf("URL length: %s\n", E3DB_Op_GetHttpUrl(op));
 				printf("Headers length: %zu\n", strlen(headers_string));
 				int ret = snprintf(request, MAX_REQUEST_SIZE,
 						   "GET %s HTTP/1.1\r\n"
@@ -438,14 +438,12 @@ int mbedtls_run_op(E3DB_Op *op)
 					exit(EXIT_FAILURE);
 				}
 			}
-
 			else
 			{
 				fprintf(stderr, "Unsupported method: %s\n", method);
 				abort();
 			}
 			printf("Request: %s\n", request);
-			printf("All headers as a string: %s\n", headers_string);
 			if (mbedtls_ssl_write(&ssl, (const unsigned char *)request, strlen(request)) != strlen(request))
 			{
 				fprintf(stderr, "Failed to set up HTTP Request.\n");
