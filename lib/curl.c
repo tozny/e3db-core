bc03bfcc47ed94a09483b8dac281ccc6c26e40f5
Katie Rosas <katie@tozny.com>
2023-12-15 16:48:56 -0800

    update

--- lib/curl.c
+++ lib/curl.c
@@ -72,11 +72,6 @@ int http_parser_status_callback(http_parser *parser, const char *at, size_t leng
 	// Convert the status code string to a long
 	response_data->response_code = parser->status_code;
 
-	// Print debug information
-	printf("Status Callback: at='%.*s', length=%zu\n", (int)length, at, length);
-	printf("Status Code: %d\n", parser->status_code);
-	printf("Response Code: %ld\n", response_data->response_code);
-
 	return 0;
 }
 
@@ -107,89 +102,8 @@ int on_headers_complete(http_parser *parser)
 http_parser_settings parser_settings = {
     .on_body = http_parser_body_callback,
     .on_status = http_parser_status_callback,
-    .on_headers_complete = on_headers_complete, // add this line
+    .on_headers_complete = on_headers_complete,
 };
-
-int curl_run_op(E3DB_Op *op)
-{
-	CURL *curl;
-	struct ResponseData response_data = {NULL, 0};
-
-	if ((curl = curl_easy_init()) == NULL)
-	{
-		fprintf(stderr, "Fatal: Curl initialization failed.\n");
-		exit(EXIT_FAILURE);
-	}
-
-	while (!E3DB_Op_IsDone(op))
-	{
-		if (E3DB_Op_IsHttpState(op))
-		{
-			curl_easy_reset(curl);
-
-			const char *method = E3DB_Op_GetHttpMethod(op);
-			E3DB_HttpHeaderList *headers = E3DB_Op_GetHttpHeaders(op);
-			struct curl_slist *chunk = NULL;
-			E3DB_HttpHeader *header = E3DB_HttpHeaderList_GetFirst(headers);
-
-			while (header != NULL)
-			{
-				char *header_text = malloc(strlen(E3DB_HttpHeader_GetName(header)) + strlen(E3DB_HttpHeader_GetValue(header)) + 3);
-				sprintf(header_text, "%s: %s", E3DB_HttpHeader_GetName(header), E3DB_HttpHeader_GetValue(header));
-				chunk = curl_slist_append(chunk, header_text);
-				// free(header_text);
-
-				header = E3DB_HttpHeader_GetNext(header);
-			}
-			printf("Header in curl  %s \n", chunk->data);
-			if (!strcmp(method, "POST"))
-			{
-				const char *post_body = E3DB_Op_GetHttpBody(op);
-				curl_easy_setopt(curl, CURLOPT_POST, 1L);
-				curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_body);
-			}
-			else if (!strcmp(method, "GET"))
-			{
-				// nothing special for GET
-			}
-			else if (!strcmp(method, "PUT"))
-			{
-				const char *put_body = E3DB_Op_GetHttpBody(op);
-				curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
-				curl_easy_setopt(curl, CURLOPT_POSTFIELDS, put_body);
-			}
-			else
-			{
-				fprintf(stderr, "Unsupported method: %s\n", method);
-				abort();
-			}
-			curl_easy_setopt(curl, CURLOPT_URL, E3DB_Op_GetHttpUrl(op));
-			// Turn on for debugging
-			// curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
-
-			CURLcode res = curl_easy_perform(curl);
-			if (res != CURLE_OK)
-			{
-				fprintf(stderr, "curl_easy_perform: %s\n", curl_easy_strerror(res));
-			}
-
-			long response_code;
-			curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-
-			// Pass the response data to E3DB_Op_FinishHttpState
-			E3DB_Op_FinishHttpState(op, response_code, response_data.data, NULL, 0);
-			// Free allocated memory
-			curl_slist_free_all(chunk);
-			free_response_data(&response_data);
-		}
-	}
-	// Free memory for response data
-	curl_easy_cleanup(curl);
-	return 0;
-}
 
 int curl_run_op_with_expected_response_code(E3DB_Op *op, long expected_response_code)
 {
@@ -304,10 +218,8 @@ int mbedtls_run_op(E3DB_Op *op)
 	mbedtls_ssl_set_bio(&ssl, &server_fd, mbedtls_net_send, mbedtls_net_recv, NULL);
 
 	struct ResponseData response_data = {NULL, 0};
-
 	http_parser parser;
 	http_parser_init(&parser, HTTP_RESPONSE);
-
 	parser.data = &response_data;
 
 	// Set hostname for Server Name Indication (SNI)
@@ -363,11 +275,8 @@ int mbedtls_run_op(E3DB_Op *op)
 		{
 			const char *method = E3DB_Op_GetHttpMethod(op);
 
-			printf("METHOD being processed %s\n", method);
-
 			E3DB_HttpHeaderList *headers = E3DB_Op_GetHttpHeaders(op);
 			size_t lengthOfhEders = E3DB_HttpHeaderList_GetLength(headers);
-			printf("HEADERS LENGTH %zu", lengthOfhEders);
 			E3DB_HttpHeader *header = E3DB_HttpHeaderList_GetFirst(headers);
 
 			// Allocate memory for headers_string and request
@@ -391,16 +300,12 @@ int mbedtls_run_op(E3DB_Op *op)
 				}
 				sprintf(header_text, "%s: %s\r\n", E3DB_HttpHeader_GetName(header), E3DB_HttpHeader_GetValue(header));
 				strcat(headers_string, header_text);
-				// free(header_text); // Free the allocated memory
 				header = E3DB_HttpHeader_GetNext(header);
 			}
 
 			if (!strcmp(method, "POST"))
 			{
 				const char *post_body = E3DB_Op_GetHttpBody(op);
-				printf("URL length: %zu\n", strlen(E3DB_Op_GetHttpUrl(op)));
-				printf("Headers length: %zu\n", strlen(headers_string));
-				printf("Post body length: %zu\n", strlen(post_body));
 				int ret = snprintf(request, MAX_REQUEST_SIZE,
 						   "POST %s HTTP/1.1\r\n"
 						   "Host: api.e3db.com\r\n" // Use only the hostname
@@ -409,27 +314,18 @@ int mbedtls_run_op(E3DB_Op *op)
 						   "%s",    // Request body
 						   E3DB_Op_GetHttpUrl(op), strlen(post_body), headers_string, post_body);
 
-				printf("Ret size %d", ret);
-				printf("snprintf returned: %d\n", ret);
 				if (ret < 0 || ret >= MAX_REQUEST_SIZE - 1) // -1 to leave room for the null terminator
 				{
 					fprintf(stderr, "Error constructing request.\n");
 					exit(EXIT_FAILURE);
 				}
-				printf("Request %s\n", request);
-				printf("URL: %s\n", E3DB_Op_GetHttpUrl(op));
-				printf("Headers: %s\n", headers_string);
-				printf("Post Body: %s\n", post_body);
 			}
 			else if (!strcmp(method, "GET"))
 			{
-				printf("URL length: %zu\n", strlen(E3DB_Op_GetHttpUrl(op)));
-				printf("URL length: %s\n", E3DB_Op_GetHttpUrl(op));
-				printf("Headers length: %zu\n", strlen(headers_string));
 				int ret = snprintf(request, MAX_REQUEST_SIZE,
 						   "GET %s HTTP/1.1\r\n"
 						   "Host: api.e3db.com\r\n" // Use only the hostname
-						   "%s\r\n"		    // Additional headers if needed
+						   "%s"			    // Additional headers if needed
 						   "\r\n",		    // No request body for GET
 						   E3DB_Op_GetHttpUrl(op), headers_string);
 				if (ret < 0 || ret >= MAX_REQUEST_SIZE - 1) // -1 to leave room for the null terminator
@@ -441,8 +337,6 @@ int mbedtls_run_op(E3DB_Op *op)
 			else if (!strcmp(method, "PUT"))
 			{
 				const char *put_body = E3DB_Op_GetHttpBody(op);
-				printf("Put Body: %s\n", put_body);
-				printf("Len Put Body: %d\n", strlen(put_body));
 
 				snprintf(request, MAX_REQUEST_SIZE,
 					 "PUT %s HTTP/1.1\r\n"
@@ -463,13 +357,13 @@ int mbedtls_run_op(E3DB_Op *op)
 				fprintf(stderr, "Unsupported method: %s\n", method);
 				abort();
 			}
-			printf("Request: %s\n", request);
 			if (mbedtls_ssl_write(&ssl, (const unsigned char *)request, strlen(request)) != strlen(request))
 			{
 				fprintf(stderr, "Failed to set up HTTP Request.\n");
 				exit(EXIT_FAILURE);
 			}
-
+			free(response_data.data);
+			response_data.data = NULL;
 			// Allocate memory for the response buffer
 			char *buffer = (char *)xmalloc(MAX_BUFFER_SIZE);
 			// Check for allocation errors
@@ -478,9 +372,7 @@ int mbedtls_run_op(E3DB_Op *op)
 				fprintf(stderr, "Memory allocation error.\n");
 				exit(EXIT_FAILURE);
 			}
-
 			int bytes_received = mbedtls_ssl_read(&ssl, (unsigned char *)buffer, MAX_BUFFER_SIZE);
-			printf("Bytes received %d\n", bytes_received);
 			if (bytes_received < 0)
 			{
 				// Handle mbedtls_ssl_read error
@@ -500,9 +392,7 @@ int mbedtls_run_op(E3DB_Op *op)
 				}
 				buffer = new_buffer;
 			}
-			printf("Bytes received %d\n", bytes_received);
-			printf("\n Buffer: %s \n", buffer);
-			printf("%s", "End of buffer");
+			// TODO Find out why the body does not get reset in response_data.data
 			if (http_parser_execute(&parser, &parser_settings, buffer, bytes_received) != bytes_received)
 			{
 				fprintf(stderr, "HTTP parsing error.\n");
@@ -513,29 +403,27 @@ int mbedtls_run_op(E3DB_Op *op)
 				mbedtls_ssl_config_free(&conf);
 				exit(EXIT_FAILURE);
 			}
+			// Intermediate fix for above issue on TODO
+			const char *body_start = strstr(buffer, "\r\n\r\n");
+			size_t body_length = strlen(body_start + 4);  // Skip the "\r\n\r\n"
+			char *body = (char *)malloc(body_length + 1); // +1 for null terminator
+			strcpy(body, body_start + 4);
 
-			printf("Buffer: %.*s\n", bytes_received, buffer);
-			printf("Response data: %s\n", response_data.data);
-			printf("Response data: %ld\n", response_data.response_code);
 			// Pass the response data to E3DB_Op_FinishHttpState
-			E3DB_Op_FinishHttpState(op, response_data.response_code, response_data.data, NULL, 0);
+			E3DB_Op_FinishHttpState(op, response_data.response_code, body, NULL, 0);
 			// Free memory at the end
 			free(headers_string);
 			free(request);
-			free(buffer);
 		}
 	}
-
+	// Free memory for response data
+	free_response_data(&response_data);
 	// Clean up mbedtls components
 	mbedtls_ssl_close_notify(&ssl);
 	mbedtls_net_free(&server_fd);
 	mbedtls_x509_crt_free(&cacert);
 	mbedtls_ssl_free(&ssl);
 	mbedtls_ssl_config_free(&conf);
-
-	// Free memory for response data
-	free_response_data(&response_data);
-	printf("%s", "Done!");
 
 	return 0;
 }
